5a74afd8d9045030a9130e94431258f6d0f6f68e
Daniele Andreotti <daniele.andreotti@cnaf.infn.it>
2012-10-30 11:57:47 +0100

    Renamed.

---
+++ src/test/java/org/italiangrid/vomsclients/VomsProxyClientsTest.java
@@ -0,0 +1,14 @@
+package org.italiangrid.vomsclients;
+
+public class VomsProxyClientsTest {
+
+	/**
+	 * @param args
+	 */
+
+	public static void main(String[] args) {
+
+		new VomsProxyInfo(args);
+	}
+
+}
