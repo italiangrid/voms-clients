134652662e9751c2bf31e4d0d466884b467d314a
Daniele Andreotti <daniele.andreotti@cnaf.infn.it>
2012-11-07 17:31:00 +0100

    Code cleanup.

--- src/main/java/org/italiangrid/vomsclients/VomsProxyInfo.java
+++ src/main/java/org/italiangrid/vomsclients/VomsProxyInfo.java
@@ -1,12 +1,19 @@
 package org.italiangrid.vomsclients;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.CommandLineParser;
 import org.apache.commons.cli.GnuParser;
 import org.apache.commons.cli.HelpFormatter;
-import org.apache.commons.cli.OptionBuilder;
+import org.apache.commons.cli.Option;
 import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
+import org.italiangrid.vomsclients.options.VomsCliOption;
+import org.italiangrid.vomsclients.options.VomsClientsCommonOptions;
+import org.italiangrid.vomsclients.options.VomsProxyInfoOptions;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -30,67 +37,28 @@ public class VomsProxyInfo {
 	private static CommandLineParser cliParser = new GnuParser();
 
 
-	@SuppressWarnings("static-access")
+	/**
+	 * Initializes command-line options
+	 */
+
 	private static void initOptions() {
 
 		cliOptions = new Options();
 
-		/* Options */
-		cliOptions.addOption("help", "usage", false, "Displays helps and exits");
-		cliOptions.addOption(OptionBuilder.withDescription("Displays version").create("version"));
-		cliOptions.addOption(OptionBuilder.hasArgs().withArgName("proxyfile")
-				.withDescription("Non-standard location of proxy").create("file"));
-		cliOptions.addOption(OptionBuilder.withDescription("Skips AC verification").create("dont_verify_ac"));
-		cliOptions.addOption(OptionBuilder.withDescription("Displays debugging output").create("debug"));
-
-		cliOptions.addOption(OptionBuilder.withDescription("Returns 0 if valid proxy exists, 1 otherwise")
-				.withArgName("[options]").hasOptionalArgs().create("exists"));
-		cliOptions.addOption(OptionBuilder.withArgName("voname")
-				.withDescription("Returns 0 if AC exists corresponding to voname, 1 otherwise").create("acexists"));
-		cliOptions.addOption(OptionBuilder.hasArgs().withDescription("Read options from file <name>").withArgName("name")
-				.create("conf"));
-		cliOptions.addOption(OptionBuilder.withDescription("Prints included file").create("included"));
+		List<VomsCliOption> listOptions = new ArrayList<VomsCliOption>();
+		listOptions.addAll(Arrays.asList(VomsClientsCommonOptions.values()));
+		listOptions.addAll(Arrays.asList(VomsProxyInfoOptions.values()));
 
 
-		/* Printout options */
-		cliOptions.addOption(OptionBuilder.withDescription("Prints information about the whole proxy chain (CA excluded)")
-				.create("chain"));
-		cliOptions.addOption(OptionBuilder.withDescription("Prints Distinguished name (DN) of proxy subject").create(
-				"subject"));
-		cliOptions.addOption(OptionBuilder.withDescription("Prints DN of proxy issuer (certificate signer)").create(
-				"issuer"));
-		cliOptions.addOption(OptionBuilder.withDescription("Prints DN of the identity represented by the proxy").create(
-				"identity"));
-		cliOptions.addOption(OptionBuilder.withDescription("Prints Type of proxy (full or limited)").create("type"));
-		cliOptions.addOption(OptionBuilder.withDescription("Prints Time (in seconds) until proxy expires").create(
-				"timeleft"));
-		cliOptions.addOption(OptionBuilder.withDescription("Prints Key size (in bits)").create("strength"));
-		cliOptions.addOption(OptionBuilder.withDescription("Prints All proxy options in a human readable format").create(
-				"all"));
-		cliOptions.addOption(OptionBuilder.withDescription("Prints All of the certificate").create("text"));
-		cliOptions.addOption(OptionBuilder.withDescription("Prints Pathname of proxy file").create("path"));
-		cliOptions.addOption(OptionBuilder.withDescription("Prints Vo name").create("vo"));
-		cliOptions.addOption(OptionBuilder.withDescription("Prints Attribute in FQAN format").create("fqan"));
-		cliOptions.addOption(OptionBuilder.withDescription("Prints Distinguished name (DN) of AC subject").create(
-				"acsubject"));
-		cliOptions.addOption(OptionBuilder.withDescription("Prints DN of AC issuer (certificate signer)")
-				.create("acissuer"));
-		cliOptions.addOption(OptionBuilder.withDescription("Prints Time (in seconds) until AC expires")
-				.create("actimeleft"));
-		cliOptions.addOption(OptionBuilder.withDescription("Prints AC serial number").create("serial"));
-		cliOptions.addOption(OptionBuilder.withDescription("Prints Server URI").create("uri"));
-		cliOptions.addOption(OptionBuilder.withDescription("Prints content of KeyUsage extension").create("keyusage"));
+		for (VomsCliOption optionElement : listOptions) {
 
+			Option option = new Option(optionElement.getOpt(), optionElement.getLongOpt(), optionElement.hasArg(),
+					optionElement.getDescription());
 
-		/* Sub options for -exists */
-		cliOptions.addOption(OptionBuilder.withDescription("[option to -exists] time requirement for proxy to be valid")
-				.withArgName("H:M").hasArgs(2).withValueSeparator(':').create("valid"));
-		cliOptions.addOption(OptionBuilder
-				.withDescription("[option to -exists] time requirement for proxy to be valid (deprecated, use -valid instead)")
-				.hasArg().withArgName("H").create("hours"));
-		cliOptions.addOption(OptionBuilder
-				.withDescription("[option to -exists] strength requirement for proxy to be valid").hasArg().withArgName("B")
-				.create("bits"));
+			option.setArgName(optionElement.getArgDescription());
+
+			cliOptions.addOption(option);
+		}
 
 	}
 
