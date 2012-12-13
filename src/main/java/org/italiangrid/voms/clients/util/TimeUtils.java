a8f52bd9cce6f6f097c725b3531c844e9d72be82
Daniele Andreotti <daniele.andreotti@cnaf.infn.it>
2012-12-13 14:45:03 +0100

    Fixed AC timeleft in case of expired time.

--- src/main/java/org/italiangrid/voms/clients/util/TimeUtils.java
+++ src/main/java/org/italiangrid/voms/clients/util/TimeUtils.java
@@ -9,34 +9,36 @@ import org.italiangrid.voms.VOMSError;
 
 public class TimeUtils {
 
-	
-	private static int getTimeIntervalInSeconds(int hours, int minutes){
-		
+	private static int getTimeIntervalInSeconds(int hours, int minutes) {
+
 		if (hours < 0)
 			throw new VOMSError("Number of hours must be a positive integer.");
-		
+
 		if (minutes < 0)
 			throw new VOMSError("Number of minutes must be a positive integer.");
-		
+
 		if (minutes > 59)
 			throw new VOMSError("Number of minutes must be in the range 0-59.");
-		
-		long timeIntervalInSeconds = TimeUnit.HOURS.toSeconds(hours) + TimeUnit.MINUTES.toSeconds(minutes);
-		
-		if (timeIntervalInSeconds > Integer.MAX_VALUE){
-			String msg = String.format("The requested lifetime is too long. The maximum value is %d hours.", TimeUnit.SECONDS.toHours(Integer.MAX_VALUE));
+
+		long timeIntervalInSeconds = TimeUnit.HOURS.toSeconds(hours)
+				+ TimeUnit.MINUTES.toSeconds(minutes);
+
+		if (timeIntervalInSeconds > Integer.MAX_VALUE) {
+			String msg = String
+					.format("The requested lifetime is too long. The maximum value is %d hours.",
+							TimeUnit.SECONDS.toHours(Integer.MAX_VALUE));
 			throw new VOMSError(msg);
 		}
-			
+
 		return (int) timeIntervalInSeconds;
-		
+
 	}
-	
+
 	public static final int parseLifetimeInHours(String lifetimeString)
 			throws ParseException {
 
 		int hours = Integer.parseInt(lifetimeString);
-		
+
 		return getTimeIntervalInSeconds(hours, 0);
 
 	}
@@ -51,7 +53,7 @@ public class TimeUtils {
 
 		int hours = Integer.parseInt(tokens[0]);
 		int minutes = Integer.parseInt(tokens[1]);
-		
+
 		return getTimeIntervalInSeconds(hours, minutes);
 
 	}
@@ -62,16 +64,25 @@ public class TimeUtils {
 	 */
 	public static final String getFormattedTime(long timeleft) {
 
-		long hours = TimeUnit.MILLISECONDS.toHours(timeleft);
+		String formattedTime = null;
 
-		long minutes = TimeUnit.MILLISECONDS.toMinutes(timeleft
-				- TimeUnit.HOURS.toMillis(hours));
+		if (timeleft <= 0)
+			formattedTime = String.format("%02d:%02d:%02d", 0, 0, 0);
+		else {
+			long hours = TimeUnit.MILLISECONDS.toHours(timeleft);
 
-		long seconds = TimeUnit.MILLISECONDS.toSeconds(timeleft
-				- TimeUnit.HOURS.toMillis(hours)
-				- TimeUnit.MINUTES.toMillis(minutes));
+			long minutes = TimeUnit.MILLISECONDS.toMinutes(timeleft
+					- TimeUnit.HOURS.toMillis(hours));
 
-		return (String.format("%02d:%02d:%02d", hours, minutes, seconds));
+			long seconds = TimeUnit.MILLISECONDS.toSeconds(timeleft
+					- TimeUnit.HOURS.toMillis(hours)
+					- TimeUnit.MINUTES.toMillis(minutes));
+
+			formattedTime = String.format("%02d:%02d:%02d", hours, minutes,
+					seconds);
+		}
+
+		return formattedTime;
 	}
 
 	public static String getACValidityAsString(VOMSAttribute attr) {
