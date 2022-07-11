f60636e168ca981fca327f19c2f910e0bc820ece
Shamrock Lee <44064051+ShamrockLee@users.noreply.github.com>
2022-07-11 16:19:32 +0800

    package/rpm, src/main/bash: lint bash scripts guided by shellcheck

--- src/main/bash/voms-proxy-destroy.sh
+++ src/main/bash/voms-proxy-destroy.sh
@@ -1,36 +1,37 @@
 #!/bin/bash
 #set -x
 
-VOMSCLIENTS_LIBS=${voms-clients.libs}
+VOMSCLIENTS_LIBS="${voms-clients.libs-}"
 
-if [ "x$VOMSCLIENTS_LIBS" == "x" ]; then
+if [ -z "$VOMSCLIENTS_LIBS" ]; then
 
   PRG="$0"
   while [ -h "$PRG" ]; do
-    ls=`ls -ld "$PRG"`
-    link=`expr "$ls" : '.*-> \(.*\)$'`
+    ls="$(ls -ld "$PRG")"
+    link="$(expr "$ls" : '.*-> \(.*\)$')"
     if expr "$link" : '/.*' > /dev/null; then
       PRG="$link"
     else
-      PRG=`dirname "$PRG"`/"$link"
+      PRG="$(dirname "$PRG")/$link"
     fi
   done
 
-  PRGDIR=`dirname "$PRG"`
+  PRGDIR="$(dirname "$PRG")"
 
-  VOMSCLIENTS_HOME=`cd "$PRGDIR/.." ; pwd`
+  VOMSCLIENTS_HOME="$(cd "$PRGDIR/.." ; pwd)"
 
-  VOMSCLIENTS_LIBS=$VOMSCLIENTS_HOME/share/java/voms-clients
+  VOMSCLIENTS_LIBS="$VOMSCLIENTS_HOME/share/java/voms-clients"
 
 fi
 
 # ':' separated list of jars, for the classpath
-VOMSCLIENTS_CP=`ls -1 $VOMSCLIENTS_LIBS/*.jar | tr '\n' ':'`
+VOMSCLIENTS_CP="$(ls -1 $VOMSCLIENTS_LIBS/*.jar | tr '\n' ':')"
 
 # the class implementing voms-proxy-destroy
 VOMSPROXYDESTROY_CLASS="org.italiangrid.voms.clients.VomsProxyDestroy"
 
 # JVM options
-VOMS_CLIENTS_JAVA_OPTIONS=${VOMS_CLIENTS_JAVA_OPTIONS:-"-XX:+UseSerialGC -Xmx16m -Djava.net.preferIPv6Addresses=true"}
+VOMS_CLIENTS_JAVA_OPTIONS="${VOMS_CLIENTS_JAVA_OPTIONS:-"-XX:+UseSerialGC -Xmx16m -Djava.net.preferIPv6Addresses=true"}"
 
-java $VOMS_CLIENTS_JAVA_OPTIONS -cp $VOMSCLIENTS_CP $VOMSPROXYDESTROY_CLASS "$@"
+# shellcheck disable=SC2086 # VOMS_CLIENTS_JAVA_OPTIONS is meant to be expanded space-delimited
+java $VOMS_CLIENTS_JAVA_OPTIONS -cp "$VOMSCLIENTS_CP" "$VOMSPROXYDESTROY_CLASS" "$@"
